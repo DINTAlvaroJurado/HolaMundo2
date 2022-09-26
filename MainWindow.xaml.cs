b6b50d224d935d27a0b567f87634d177abda12d1
alumno <alumno@A302PC36>
2022-09-26 13:45:48 +0200

    primer commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace HolaMundo
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void saludarButton(object sender, RoutedEventArgs e)
+        {
+            nombreTextBlock.Text = "Hola " + nombreTextBox.Text;
+        }
+    }
+}
